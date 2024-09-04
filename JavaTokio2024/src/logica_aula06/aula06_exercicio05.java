9c1b90b28c8a0938bef2cd2e01c0c3c87dc0f1c2
Matheus Alencar <alencarmatheus278@gmail.com>
2024-09-04 14:18:58 -0300

    commit de atualização

--- JavaTokio2024/src/logica_aula06/aula06_exercicio05.java
+++ JavaTokio2024/src/logica_aula06/aula06_exercicio05.java
@@ -1,10 +1,19 @@
 package logica_aula06;
 
+import java.util.Scanner;
+
 public class aula06_exercicio05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite um numero: ");
+		float num1 = sc.nextFloat();
+		
+		System.out.println("Digite um numero 2:");
+		float num2 = sc.nextFloat();
+		
+		System.out.println("Digite uns dos caracteres \n +  -  *  /");
+		char operacao = 
 	}
 
 }
